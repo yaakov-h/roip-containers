96c493341d76036c31df1682a4d388241aa23396
Yaakov <git@yaakov.dev>
2022-07-24 00:28:52 +1000

    cleanup

--- analog-reflector/publish.sh
+++ analog-reflector/publish.sh
@@ -5,4 +5,3 @@ COMMIT=$(cd ${BASEDIR}/src && git rev-parse HEAD)
 docker push registry.yaakov.online/public/roip-analog-reflector:$COMMIT
 docker image tag registry.yaakov.online/public/roip-analog-reflector:$COMMIT registry.yaakov.online/public/roip-analog-reflector:latest
 docker push registry.yaakov.online/public/roip-analog-reflector:latest
-
